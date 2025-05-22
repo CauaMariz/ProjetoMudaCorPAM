3bd873122b80eb645859bb149e0586a0bd4b504f
Caua Mariz DEV <marizcaua88@gmail.com>
2025-05-22 15:28:43 -0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Etim PAM1 CAUAM Preferencias de Cores"
+include(":app")
+ 
